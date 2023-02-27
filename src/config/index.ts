8bda5eabafb4ab52bcc64579f5992f4cf8b445eb
Amine ELMAROUFI <elmaroufi.amine@gmail.com>
2023-02-27 17:41:14 +0100

    update url api to prod

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 // export const baseURL = "http://localhost:8000/api/v1";
 //PROD
 // export const baseURL = "https://bookingsapi-app.herokuapp.com/api/v1";
-// export const baseURL = "https://bookingapi-app.onrender.com/api/v1";
+export const baseURL = "https://bookingapi-app.onrender.com/api/v1";
 
 //LOCAL
-export const baseURL = "http://localhost:8000/api/v1";
+// export const baseURL = "http://localhost:8000/api/v1";

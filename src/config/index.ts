7d19ba918ce2a688218f940b54a7e7237737997c
EL MAROUFI Amine <aminelmaroufi@users.noreply.github.com>
2023-09-04 13:09:39 +0200

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 //PROD
-export const baseURL = "https://bookingapi-app.onrender.com/api/v1";
+// export const baseURL = "" ;
 // export const baseURL = `${process.env.REACT_APP_API_URL}/api/v1`;
 //LOCAL
-// export const baseURL = "http://localhost:8000/api/v1";
+export const baseURL = "http://localhost:8000/api/v1";
